ddbcf44c819af0cf27ba6ff7604fdd7c3ceadbf7
michael172849 <mich94hj@utexas.edu>
2019-12-08 22:59:57 -0600

    script

--- src/run_multi.sh
+++ src/run_multi.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 for i in {0..5}
 do
-    python3 main.py "oven sarsa" > results/ocse${i}.txt &
+    python3 main.py "oven coffee sarsa extraFeature" > results/ocse${i}.txt &
+done
+wait
+for i in {0..5}
+do
+    python3 main.py "oven coffee sarsa" > results/ocs${i}.txt &
 done
